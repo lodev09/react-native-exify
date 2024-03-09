d9114e3ae9872a203f0406658b178cd8293f1b4a
lodev09 <lodev09@gmail.com>
2024-03-09 17:57:11 +0800

    chore: metro

--- example/metro.config.js
+++ example/metro.config.js
@@ -13,7 +13,7 @@ const modules = Object.keys({ ...pak.peerDependencies })
  * Metro configuration
  * https://facebook.github.io/metro/docs/configuration
  *
- * @type {import('metro-config').MetroConfig}
+ * @type {import('expo/metro-config').MetroConfig}
  */
 const config = {
   watchFolders: [root],
